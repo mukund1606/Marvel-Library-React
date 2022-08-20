39c9f2b2bbd2f22bf5a0d45290c83ab2a512da89
Mukune Mittal <mukund.mukulmittal@gmail.com>
2022-08-21 01:33:14 +0530

    Thanos Added

--- src/Avengers.js
+++ src/Avengers.js
@@ -5,27 +5,34 @@ function Avengers(props) {
     let avengers = [
         {
             name: "IronMan",
-            realname: "Tony Stark",
+            charname: "Tony Stark",
             imageURL: "https://i.pinimg.com/564x/d7/bd/93/d7bd934adc5e5a50da570cb1bc98e946.jpg",
             imgalt: "IronMan"
         },
         {
             name: "SpiderMan",
-            realname: "Peter Parker",
+            charname: "Peter Parker",
             imageURL: "https://i.pinimg.com/564x/bc/4c/a5/bc4ca55a187c1ec417fb254e580918a5.jpg",
             imgalt: "SpiderMan"
         },
         {
             name: "Thor",
-            realname: "Thor",
+            charname: "Thor",
             imageURL: "https://i.pinimg.com/564x/6d/ab/d2/6dabd294da8f4a3ce9c739d91ada3d2a.jpg",
             imgalt: "Thor"
         },
         {
             name: "Captain America",
-            realname: "Steve Rogers",
+            charname: "Steve Rogers",
             imageURL: "https://i.pinimg.com/564x/09/29/eb/0929ebfc354683eb818fc143d23f9bf3.jpg",
             imagealt: "Captain America"
+        },
+        {
+            name: "Thanos",
+            charname: "Thanos",
+            imageURL: "https://i.pinimg.com/736x/e8/16/59/e81659c3ff56354819a45ca49b265a63.jpg",
+            imagealt: "Thanos"
+
         }
     ]
 
@@ -37,7 +44,7 @@ function Avengers(props) {
                         <div key={index} className='avenger'>
                             <img className="avenger-image" src={avenger.imageURL} alt={avenger.imgalt} />
                             <h1 className='avenger-name'>{avenger.name}</h1>
-                            <h2 className='avenger-realname'>{avenger.realname}</h2>
+                            <h2 className='avenger-realname'>{avenger.charname}</h2>
                         </div>
                     )
                 })
